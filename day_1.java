621de50834eba220d0e556fd475901bac78c73e9
gibranghadavi <gibranghadavi@gmail.com>
2022-02-19 18:08:43 +0700

    day 1

---
+++ day_1.java
@@ -0,0 +1,8 @@
+package Belajarjava;
+
+public class day_1 {
+    public static void main (String [] args){
+        System.out.println ("hello world");
+        System.out.println ("hello majene");  
+    } 
+}
