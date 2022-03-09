c180b1240b4d5ae30b8c9ad93180960b5cb951ee
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-03-09 20:29:58 +0800

    Add files via upload

---
+++ Day19_Java_String_Tokons.java
@@ -0,0 +1,20 @@
+
+package gibranghadavi;
+import java.io.*;
+import java.util.*;
+
+public class Day19_Java_String_Tokons {
+     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.replaceAll("[!,?._'@\\s]+", "  ");
+        StringTokenizer st = new StringTokenizer(s);
+        int n = st.countTokens();
+        System.out.println(n);
+        for(int i=0;i<n;i++){
+            System.out.println(st.nextToken());
+        }
+        scan.close();
+    }
+}
