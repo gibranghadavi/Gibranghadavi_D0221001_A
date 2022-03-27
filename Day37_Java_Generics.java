cf49175d49a634c2a1300b4318091e2c4d287b94
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-03-27 23:08:23 +0800

    Add files via upload

---
+++ Day37_Java_Generics.java
@@ -0,0 +1,12 @@
+
+package gibranghadavi;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class Day37_Java_Generics {
+    public static void main(String []argh){
+        for (T x:array){
+            System.out.println(x);
+        }
+    }
+}
