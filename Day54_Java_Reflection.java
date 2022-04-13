21a1a3f2496edf692a0eee6dbe96346503f0a7ab
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-04-13 22:00:35 +0800

    Add files via upload

---
+++ Day54_Java_Reflection.java
@@ -0,0 +1,20 @@
+
+package gibranghadavi;
+import java.io.*;
+import java.util.*;
+
+public class Day54_Java_Reflection {
+    public static void main(String[] args){
+        Class student = new Student().getClass();
+        Method[] methods = student.getDeclaredMethods();
+        
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method:methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+}
