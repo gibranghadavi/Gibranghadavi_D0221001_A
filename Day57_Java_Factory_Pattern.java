b127e9456e57c3f75139cbb25b7912aa5ae1dd47
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-04-16 20:10:57 +0800

    day coding

---
+++ Day57_Java_Factory_Pattern.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gibranghadavi;
+import java.util.*;
+import java.security.*;
+interface Food {
+	 public String getType();
+	}
+	class Pizza implements Food {
+	 public String getType() {
+	 return "Someone ordered a Fast Food!";
+	 }
+	}
+
+	class Cake implements Food {
+
+	 public String getType() {
+	 return "Someone ordered a Dessert!";
+	 }
+	}
+	class FoodFactory {
+		public Food getFood(String order) {
+switch (order){
+     case "pizza": return new Pizza();
+     case "cake" : return new Cake();
+     default : return null;
+}
+
+
+}//End of getFood method
+
+	}//End of factory class
+
+public class Day57_Java_Factory_Pattern {
+     public static void main(String args[]){
+			Do_Not_Terminate.forbidExit();
+
+		try{
+
+			Scanner sc=new Scanner(System.in);
+			//creating the factory
+			FoodFactory foodFactory = new FoodFactory();
+	
+			//factory instantiates an object
+			Food food = foodFactory.getFood(sc.nextLine());
+	
+			
+			System.out.println("The factory returned "+food.getClass());
+			System.out.println(food.getType());
+		}
+		catch (Do_Not_Terminate.ExitTrappedException e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	 }
+
+	}
+	class Do_Not_Terminate {
+		 
+	    public static class ExitTrappedException extends SecurityException {
+
+			private static final long serialVersionUID = 1L;
+	    }
+	 
+	    public static void forbidExit() {
+	        final SecurityManager securityManager = new SecurityManager() {
+	            @Override
+	            public void checkPermission(Permission permission) {
+	                if (permission.getName().contains("exitVM")) {
+	                    throw new ExitTrappedException();
+	                }
+	            }
+	        };
+	        System.setSecurityManager(securityManager);
+	    }
+	}	
+		
+
+
+
+
+
